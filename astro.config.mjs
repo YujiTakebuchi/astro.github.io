60a566d19bba63c7fe83b9415c1af62141234941
YujiTakebuchi <takeyou.swift@gmail.com>
2023-03-28 00:28:24 +0900

    GitHub pages デプロイテスト、baseディレクトリ名変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yujitakebuchi.github.io",
-  base: "/astro.github.io",
+  base: "/astro",
 });
